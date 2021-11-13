6c20e0a58900c01271af6eda8874f29fce5c2c71
pratheek-yalla <86199188+pratheek-yalla@users.noreply.github.com>
2021-11-13 21:18:49 +0530

    check

---
+++ aim.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Microstructural analysis of cast-iron specimens</title>
+        <link rel="icon" href="images/nitw.png">
+        <link rel="stylesheet" href="css/style.css" type="text/css">
+    </head>
+    <body>
+        <a href="./"><img  class="logo" src="images/headerlogo_mme.png"></a>
+        <div class="menubox">
+            <div class="menu">
+                <span><a href="./aim.html">AIM</a></span>
+                <a href="./theory.html">THEORY</a>
+                <a href="./procedure.html">PROCEDURE</a>
+                <a href="./precautions.html">PRECAUTIONS</a>
+                <a href="./simulator.html">SIMULATOR</a>
+                <a href="./quiz.html">QUIZ</a>
+                <a href="./references.html">REFERENCES</a>
+            </div>
+        </div>
+        <h2 align="center">Microstructural analysis of cast-iron specimens</h2>
+        <hr width="90%">
+        <div class="body">
+            <ul>
+                <li><h2>Aim</h2></li>
+                <li>Learn the microstructural features of cast iron.</li>
+                <li>Analyze the microstructure of cast iron applying quantitative metallography principles.</li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p><u><b>Contributors:</b></u>&nbsp; Abhayraj(205101) | Divya Sree(205116) | Rohith Kumar(205132) | Ratnesh Kumar(205152) | Yalla Pratheek(205170)</p>
+        </div>
+    </body>
+</html>
