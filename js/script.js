8d1fa1a6b2de70648995ad2c340ba0b3c5fb2705
pratheek-yalla <86199188+pratheek-yalla@users.noreply.github.com>
2021-11-28 23:14:23 +0530

    done simulator

--- js/script.js
+++ js/script.js
@@ -2,3 +2,23 @@ function show() {
     document.getElementById("next").style.display = "block";
     document.getElementById("gif").style.display = "block";
 }
+function zoomin() {
+    document.getElementById("image").style.transform = "scale(2.5)";
+}
+function zoomout() {
+    document.getElementById("image").style.transform = "scale(1)";
+}
+
+let random = Math.floor(Math.random() * 5) + 1;
+document.getElementById("image").src = "./images/image" + random + ".jpg";
+
+function checkAnswer() {
+    var index = document.getElementById("microstructure").value;
+    if(index == random) {
+        document.getElementById("answer").innerHTML = "Correct Answer!";
+    }
+    else {
+        document.getElementById("answer").innerHTML = "Wrong Answer!";
+    }
+    document.getElementById("answer").style.display = "block";
+}
